1574f10dbbad9acd89c6273c473a320f154ef5ec
Song-ki-beom <63495505+Song-ki-beom@users.noreply.github.com>
2021-11-13 23:45:16 +0900

    스크롤바 휠 반응 및 이벤트 텍스트 잘림 수정
    ㅇ

--- Assets/KiboemScript/ScrollBar.cs
+++ Assets/KiboemScript/ScrollBar.cs
@@ -5,12 +5,28 @@ using UnityEngine.UI;
 
 public class ScrollBar : MonoBehaviour
 {
+    [SerializeField]
+    private float offset = 3.4f;
     public Slider slider;
     public ScrollRect scrollRect;
+    
     // Use this for initialization
+    // float wheelInput = Input.GetAxis("Mouse ScrollWheel");
+    
 
+    
+    
+    void Update()
+    {
+        Vector3 wheelInput = Input.mouseScrollDelta;
+        if (wheelInput.y !=0)
+            slider.value +=wheelInput.y*Time.deltaTime*offset;
+        
+    }
     public void ChangeScrollPos()
     {
+
         scrollRect.verticalNormalizedPosition = slider.value;
+        
     }
 }
