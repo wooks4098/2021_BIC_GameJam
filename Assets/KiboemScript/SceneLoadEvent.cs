bd6f2830ace6b5558f6c0781866dd84b6027a6a7
Song-ki-beom <63495505+Song-ki-beom@users.noreply.github.com>
2021-11-14 00:51:18 +0900

    튜토리얼 씬 팝업
    11

---
+++ Assets/KiboemScript/SceneLoadEvent.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class SceneLoadEvent : MonoBehaviour
+{
+    
+    [SerializeField]
+    private Canvas TutorialCanvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        DontDestroyOnLoad(this.gameObject);
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+    void OnSceneLoaded(Scene scene , LoadSceneMode mode)
+    {
+        Instantiate(TutorialCanvas);
+        TutorialCanvas.gameObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
